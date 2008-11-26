f3b6e170c60d6e4b9e462d67240c207116f9130f
lakshan <lakshan@web2media.net>
2008-11-26 06:30:21 +0530

    Integrated authlogic basics

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
   include ExceptionNotifiable
-  include AuthenticatedSystem
-  include RoleRequirementSystem
+  #include AuthenticatedSystem
+  #include RoleRequirementSystem
 
   helper :all # include all helpers, all the time
+  helper_method :current_user_session, :current_user
+  
   protect_from_forgery :secret => 'b0a876313f3f9195e9bd01473bc5cd06'
   filter_parameter_logging :password, :password_confirmation
   rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
@@ -14,5 +16,44 @@ class ApplicationController < ActionController::Base
   def record_not_found
     render :file => File.join(RAILS_ROOT, 'public', '404.html'), :status => 404
   end
+  
+ private
+    def current_user_session
+      return @current_user_session if defined?(@current_user_session)
+      @current_user_session = UserSession.find
+    end
+
+    def current_user
+      return @current_user if defined?(@current_user)
+      @current_user = current_user_session && current_user_session.user
+    end
+    
+    def require_user
+      unless current_user
+        store_location
+        flash[:notice] = "You must be logged in to access this page"
+        redirect_to new_user_session_url
+        return false
+      end
+    end
+
+    def require_no_user
+      if current_user
+        store_location
+        flash[:notice] = "You must be logged out to access this page"
+        redirect_to account_url
+        return false
+      end
+    end
+
+    def store_location
+      session[:return_to] = request.request_uri
+    end
+
+    def redirect_back_or_default(default)
+      redirect_to(session[:return_to] || default)
+      session[:return_to] = nil
+    end
+    
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ ActionController::Routing::Routes.draw do |map|
   #map.open_id_create '/opencreate', :controller => "users", :action => "create", :requirements => { :method => :get }
   
   # Restful Authentication Resources
+  map.resource :account, :controller => "users"
   map.resources :users
   map.resources :passwords
   #map.resource :session
